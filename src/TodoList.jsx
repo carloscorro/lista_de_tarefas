a49195f36f24abc82d558a4a42d265e66f1ca5f4
Carlos Corro <c.e.mercedescorro@hotmail.com>
2023-11-29 22:38:45 -0300

    first_commit

---
+++ src/TodoList.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import './TodoList.css';
+
+function TodoList(){
+    return (
+    <div>
+        <h1>Lista de Tarefas</h1>
+        <form>
+            <input 
+                type="text" 
+                placeholder= "Adicione uma tarefa"
+            />
+            <button className="add" type="submit">Add</button>
+        </form>
+        <div className="listaTarefas">
+            <div className="item">
+                <span>Tarefa de Exemplo</span>
+                <button className="del">Deletar</button>
+            </div>
+            <div className="item completo">
+                <span>Tarefa de Exemplo</span>
+                <button className="del">Deletar</button>
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default TodoList
